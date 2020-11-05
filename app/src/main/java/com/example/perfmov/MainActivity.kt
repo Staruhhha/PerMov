b5f03b688a8116556ce5bbb401f843c2be0d97d5
Staruhhha <mike04feed@gmail.com>
2020-11-05 14:52:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/perfmov/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.perfmov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.perfmov.fragments.ReleasesFragment
+import com.example.perfmov.fragments.TopFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val releasesFragment = ReleasesFragment()
+        val topFragment = TopFragment()
+
+        makeCurrentFragment(releasesFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.newReleases -> makeCurrentFragment(releasesFragment)
+                R.id.topFilms -> makeCurrentFragment(topFragment)
+            }
+            true 
+        }
+    }
+
+    private fun makeCurrentFragment (fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameSw, fragment)
+            commit()
+        }
+}
